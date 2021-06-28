e3ebcbafb4c8492a6d3fa5fce053eb166a0a41de
SkillFulDev <chernonog.evgeniy@gmail.com>
2021-06-28 18:22:46 +0300

    Update Program.cs

--- CH_TheAddressBook/Program.cs
+++ CH_TheAddressBook/Program.cs
@@ -7,25 +7,45 @@ namespace CH_TheAddressBook
         static void Main(string[] args)
         {
             Console.Write("Please, input number of contacts you need:");
-            int count = int.Parse(Console.ReadLine());
-            string[] names = new string[count];
-            int[] phoneNumbers = new int[count];
-            for (int i = 0; i < names.Length; i++)
+            int count;
+            string str = Console.ReadLine();
+            bool countTryParse = int.TryParse(str, out count);
+            if (countTryParse)
             {
-                Console.Write("Input  name: ");
-                names[i] = Console.ReadLine();
-                Console.Write("Input  phone number:");
-                phoneNumbers[i] = int.Parse(Console.ReadLine());
+                string[] names = new string[count];
+                int[] phoneNumbers = new int[count];
+                for (int i = 0; i < names.Length; i++)
+                {
+                    Console.Write("Input  name: ");
+                    names[i] = Console.ReadLine();
+                    Console.Write("Input  phone number:");
+                    string srtPhoneNumber = Console.ReadLine();
+                    int PhoneNumber;
+                    bool isPhoneCorrectlyFormat = int.TryParse(srtPhoneNumber, out PhoneNumber);
 
+                    if (isPhoneCorrectlyFormat)
+                    {
+                        phoneNumbers[i]=PhoneNumber;
+                    }
+                    else
+                    {
+                        Console.WriteLine("It isnt correctly Phone number");
+                    }
+
+                }
+                Console.WriteLine("");
+                for (int i = 0; i < count; i++)
+                {
+                    Console.WriteLine("Name: " + names[i] + "Phone nubmer: " + phoneNumbers[i]);
+                }
             }
-            Console.WriteLine("");
-            for (int i = 0; i < count; i++)
+            else
             {
-                Console.WriteLine("Name: " + names[i] + "Phone nubmer: " + phoneNumbers[i]);
-
+                Console.WriteLine("It isn`t a number");
             }
+            Console.ReadLine();
 
-            Console.ReadKey();
+
         }
     }
 }
